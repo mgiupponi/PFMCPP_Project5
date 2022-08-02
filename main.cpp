688e02154b3d99fbdd7d77c82d043b8797b6d367
mgiupponi <63476548+mgiupponi@users.noreply.github.com>
2022-08-02 03:10:30 +0000

    some progress

--- main.cpp
+++ main.cpp
@@ -71,17 +71,272 @@ write 3 UDTs below that EACH have:
 
  */
 
+#include <iostream>
+
 /*
  copied UDT 1:
  */
+struct StationersShop      
+{
+    int numPaperSizesForsale = 15;    
+    int numWrappingPaperDesignsForsale = 20;        
+    float monthElectricityBill;            
+    float profitPerDay;               
+    int numberOfEmployees;
+
+    StationersShop() : monthElectricityBill( 138.2f ), profitPerDay( 138.2f ), numberOfEmployees( 10 ) { }
+    
+    struct Paper
+    {
+        bool forPhotoPrinting = false;
+        float thicnessInuM = 200.0f;        
+        float priceInUsd = 0.1f;
+        std::string sizeStandard = "A4";
+        std::string manufacturer = "whixe";
+
+        void printDocument(std::string fileName, float zoomPercentage, bool onesided = false); 
+        float printPhoto(int qualityIndex);
+        bool moveToSellingOutbox(Paper paperB);
+        
+    };
+
+    float makeAPhotocopy(Paper paperA, bool isBlackAndWhite = true , bool onesided = false);
+    float takePassportPhoto(Paper paperB, std::string sizeStandard);
+    void wrapAGift(std::string wrappingPaperModel);
+    float chargeCustomer(std::string customerName, float maxCredit, int numItems);
+    float sendItems(std::string customerAddress, float weightPerItem, int numItems);
+
+    Paper paperToBeUsed;  
+};
+
+float StationersShop::makeAPhotocopy(Paper paperA, bool isBlackAndWhite, bool onesided)
+{    
+    float price = paperA.priceInUsd;
+    if (isBlackAndWhite == false)
+    {
+         price = price * 2;
+    }
+    if (onesided == false)
+    {
+         price = price * 2;
+    }
+    std::cout << "Making a photocopy!" << std::endl;
+    std::cout << "StationersShop::makeAPhotocopy(...) monthElectricityBill:" << monthElectricityBill << " numberOfEmployees: " << numberOfEmployees << std::endl;
+    return price;
+}
+
+float StationersShop::takePassportPhoto(Paper paperB, std::string sizeStandard)
+{    
+    float price = paperB.priceInUsd;
+    if (sizeStandard.length() > 2)
+    {
+         price = price * 2;
+    }
+    return price;
+}
+
+void StationersShop::wrapAGift(std::string wrappingPaperModel)
+{    
+    std::cout << wrappingPaperModel.size();
+}
+
+float StationersShop::chargeCustomer(std::string customerName, float maxCredit, int numItems)
+{    
+    float totalPrice = 0.0f;
+    auto paper = StationersShop::Paper();
+    while ( totalPrice < maxCredit ) 
+    {
+        paper.priceInUsd = ( maxCredit / numItems ) * 0.95f - paper.priceInUsd * 0.2f;
+        totalPrice += paper.priceInUsd * 1.2f;
+        std::cout << "chargeCustomer customerName:" << customerName << " totalPrice: " << totalPrice << std::endl;
+    }
+    return totalPrice;
+}
+
+
+float StationersShop::sendItems(std::string customerAddress, float weightPerItem, int numItems)
+{   
+    float shippingCost = 0.0f;
+    auto paper = StationersShop::Paper();
+    int i = 0;
+    while ( i < numItems ) 
+    {
+        paper.thicnessInuM = ( paper.thicnessInuM ) * 0.95f + paper.thicnessInuM * 0.2f;
+        shippingCost += paper.thicnessInuM * 0.8f + weightPerItem;
+        std::cout << "sendItems customerAddress:" << customerAddress << " shippingCost: " << shippingCost << std::endl;
+        ++i;
+    }
+    return shippingCost;
+}
+
 
 /*
  copied UDT 2:
  */
 
+struct Wallet     
+{
+    int numSlots;  
+    int numCreditCards = 4;     
+    float amountOfCashCarried = 250.0f;            
+    float weightInGrams = 150.5f;               
+    std::string color = "Brown";
+
+    Wallet() : numSlots( 7 ), color( "Brown" ) { }
+    
+    struct CreditCard      
+    {
+        bool isInternational = true;
+        float widthInCm = 200.0f;
+        double cardNumber = 23344331776;
+        float availableCreditInUsd = 1400.0f;
+        std::string bank = "HSBC";
+        std::string program = "flyingSilver";
+
+        int payInPOS(bool useChip = true);
+        std::string payOnline(std::string url, bool requiresToken = false);
+        float getAvailableCredit();
+    };
+
+    std::string identifyTheOwner(CreditCard creditCardA);
+    bool payLunch(CreditCard creditCardB, float tipPercentage = 10.0);
+    bool enterABuilding(float walletOrientationToUseInDegrees = 0.0f);
+    float creditAuthorization(CreditCard creditCardB, float pricePerItem, int numItems);
+    float creditCardBalance(CreditCard creditCardB, int maxEntries);
+    
+    CreditCard creditCardSelected;
+};
+
+std::string Wallet::identifyTheOwner(CreditCard creditCardA)
+{   
+    std::cout << "Owner being identified!" << std::endl;
+    std::cout << "Wallet::identifyTheOwner(...) numSlots:" << numSlots << " numCreditCards: " << numCreditCards << std::endl;
+    return std::to_string(creditCardA.cardNumber);
+}
+
+bool Wallet::payLunch(CreditCard creditCardB, float tipPercentage)
+{    
+    if (creditCardB.availableCreditInUsd*tipPercentage > 50)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool Wallet::enterABuilding(float walletOrientationToUseInDegrees)
+{    
+    if (walletOrientationToUseInDegrees > 45)
+    {
+        return true;
+    }
+    return false;
+}
+
+float Wallet::creditAuthorization(CreditCard creditCardB, float pricePerItem, int numItems)
+{    
+    float amountReserved = 0.0f;
+    for (int i = 0; i < numItems; ++i)
+    {
+        creditCardB.availableCreditInUsd = ( creditCardB.availableCreditInUsd ) * 0.7f + pricePerItem;
+        amountReserved += creditCardB.availableCreditInUsd * 1.01f;
+        std::cout << "creditAuthorization cardNumber:" << creditCardB.cardNumber << " availableCreditInUsd: " << creditCardB.availableCreditInUsd << std::endl;
+    }
+    return amountReserved;
+}
+
+float Wallet::creditCardBalance(CreditCard creditCardB, int maxEntries)
+{    
+    float finalBalance = 0.0f;
+    for (int i = 0; i < maxEntries; ++i) 
+    {
+        creditCardB.availableCreditInUsd *= 0.9f;
+        finalBalance += creditCardB.availableCreditInUsd * 1.05f;
+        std::cout << "creditCardBalance cardNumber:" << creditCardB.cardNumber << " finalBalance: " << finalBalance << std::endl;
+    }
+    return finalBalance;
+}
+
 /*
  copied UDT 3:
  */
+
+struct Laptop   
+{
+    std::string color;
+    float weightInGrams = 150.5f;     
+    std::string processorModel = "Corei3";              
+    int screenSizeInInches;               
+    std::string brand = "Lenovo";
+
+    Laptop() : color( "Black" ), screenSizeInInches( 17 ) { }
+    
+    struct File      
+    {
+        bool isReadOnly = false;
+        float sizeInBytes = 2000.0f;        
+        std::string fullPath = "/root/file.jpg";
+        std::string fileType = "image";
+
+        std::string readFile(int startPosition, int endPosition);
+        void replaceFile(std::string content);
+        bool deleteFile(bool askForConfirmation);
+    };
+
+    std::string readDocument(File inputFile);
+    float surfTheWeb(std::string url = "www.google.com", bool incognitoMode = false);
+    void TakeAPicture(File outputFile, float lidOrientationInDegrees = 55.0f);
+    float defragmentFile(File inputFile, float precision);
+    float zoomFile(File inputFile, float zoomPercentage);
+
+    File fileBeingProcessed;
+};
+
+std::string Laptop::readDocument(File inputFile)
+{    
+    std::cout << "Starting to read document!" << std::endl;
+    std::cout << "Laptop::readDocument(...) color:" << color << " weightInGrams: " << weightInGrams << std::endl;
+    return inputFile.fullPath;
+}
+
+float Laptop::surfTheWeb(std::string url, bool incognitoMode)
+{    
+    float output = url.length();
+    if (incognitoMode)
+    {
+         output *= 2;
+    }
+    return output;
+}
+
+void Laptop::TakeAPicture(File outputFile, float lidOrientationInDegrees)
+{    
+    if (lidOrientationInDegrees > 5)
+        outputFile.fullPath.size();
+}
+
+float Laptop::defragmentFile(File inputFile, float precision)
+{    
+    float achievedPrecision = 0.0f;
+    while ( achievedPrecision < precision ) 
+    {
+        inputFile.sizeInBytes *= 0.8f;
+        achievedPrecision += 2.0f ;
+        std::cout << "defragmentFile fullPath:" << inputFile.fullPath << " achievedPrecision: " << achievedPrecision << std::endl;
+    }
+    return achievedPrecision;
+}
+
+float Laptop::zoomFile(File inputFile, float zoomPercentage)
+{    
+    float currentZoom = 100.0f;
+    while ( currentZoom < zoomPercentage ) 
+    {
+        inputFile.sizeInBytes *= 1.1f;
+        currentZoom += inputFile.sizeInBytes * 0.02f;
+        std::cout << "zoomFile fullPath:" << inputFile.fullPath << " sizeInBytes: " << inputFile.sizeInBytes << std::endl;
+    }
+    return currentZoom;
+}
 
 /*
  new UDT 4:
